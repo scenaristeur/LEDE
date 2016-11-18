d998be72d3dea51b6ed67a90985572df0937918f
Arturo Rinaldi <arty.net2@gmail.com>
2016-11-18 17:58:22 +0100

    default option folder is now named .lede

--- include/toplevel.mk
+++ include/toplevel.mk
@@ -94,7 +94,7 @@ prepare-tmpinfo: FORCE
 
 .config: ./scripts/config/conf $(if $(CONFIG_HAVE_DOT_CONFIG),,prepare-tmpinfo)
 	@+if [ \! -e .config ] || ! grep CONFIG_HAVE_DOT_CONFIG .config >/dev/null; then \
-		[ -e $(HOME)/.openwrt/defconfig ] && cp $(HOME)/.openwrt/defconfig .config; \
+		[ -e $(HOME)/.lede/defconfig ] && cp $(HOME)/.lede/defconfig .config; \
 		$(_SINGLE)$(NO_TRACE_MAKE) menuconfig $(PREP_MK); \
 	fi
 
@@ -114,7 +114,7 @@ config-clean: FORCE
 
 defconfig: scripts/config/conf prepare-tmpinfo FORCE
 	touch .config
-	@if [ ! -s .config -a -e $(HOME)/.openwrt/defconfig ]; then cp $(HOME)/.openwrt/defconfig .config; fi
+	@if [ ! -s .config -a -e $(HOME)/.lede/defconfig ]; then cp $(HOME)/.lede/defconfig .config; fi
 	$< --defconfig=.config Config.in
 
 deflinino: scripts/config/conf prepare-tmpinfo FORCE
@@ -131,8 +131,8 @@ oldconfig: scripts/config/conf prepare-tmpinfo FORCE
 	$< --$(if $(confdefault),$(confdefault),old)config Config.in
 
 menuconfig: scripts/config/mconf prepare-tmpinfo FORCE
-	if [ \! -e .config -a -e $(HOME)/.openwrt/defconfig ]; then \
-		cp $(HOME)/.openwrt/defconfig .config; \
+	if [ \! -e .config -a -e $(HOME)/.lede/defconfig ]; then \
+		cp $(HOME)/.lede/defconfig .config; \
 	fi
 	$< Config.in
 
